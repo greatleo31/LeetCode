abfd6fcf90ffb626b0e45a8e73ff4ea696e4ea1b
yankehu <joechon31@gmail.com>
2026-01-27 21:54:32 +0800

    Day6--283 移动零

---
+++ src/com/coder/doublePoint/283-MoveZero/DoublePoint.java
@@ -0,0 +1,20 @@
+package com.coder.doublePoint;
+public class DoublePoint {
+    public void moveZeroes(int[] nums) {
+        //双指针算法：一个指针遍历，一个指针指向目标数组
+
+        //初始化指针
+        int object = 0;
+
+        //遍历
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 0) continue;
+
+            nums[object++] = nums[i];
+        }
+        //赋零
+        for(int i=object; i<nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
